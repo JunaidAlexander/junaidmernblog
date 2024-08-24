e31fea01fa0e9d59d8c750dd7c9be7111cd1c1f3
Junaid Alexander <junaidalexander76@gmail.com>
2024-08-24 16:03:08 +0100

    finished the blog
    Signed-off-by: Junaid Alexander <junaidalexander76@gmail.com>

--- Backend/server.js
+++ Backend/server.js
@@ -1,101 +1,110 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const Blog = require("./models/blogSchema");
+require('dotenv').config(); // This must be the first line in your server.js  
+const express = require("express");  
+const mongoose = require("mongoose");  
+const cors = require("cors");  
+const bodyParser = require("body-parser");  
+const Blog = require("./models/blogSchema");  
 
-// connect to express app
-const app = express();
+const app = express();  
 
-// connect to mongoDB
-const dbURI = `mongodb+srv://siyamhlawuli13:blog-mern@cluster0.9ok00cy.mongodb.net/mern-blog?retryWrites=true&w=majority`;
-// mongoose
-	.connect(dbURI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
-	.then(() => {
-		app.listen(3001, () => {
-			console.log("Server and MongoDB are connected");
-		});
-	})
-	.catch(error => {
-		console.log("Unable to connect to Server and MongoDB:", error);
-	});
+// Connect to MongoDB  
+const dbURI = process.env.DB_URI;  // Fetch the DB_URI from environment variables  
+console.log("Database URI:", dbURI); // Log to verify it's being read correctly  
 
-//middleware
+mongoose  
+    .connect(dbURI, {  
+        useNewUrlParser: true,  
+        useUnifiedTopology: true  
+    })  
+    .then(() => {  
+        app.listen(process.env.PORT || 3001, () => {  
+            console.log("Server and MongoDB are connected");  
+        });  
+    })  
+    .catch(error => {  
+        console.error("Unable to connect to Server and MongoDB:", error);  
+    });  
+// middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-// POST
-// Create; // POST
-// Read; // GET
-// Update; // PUT OR PATCH
-// Delete; // DELETE
-
-// Routes
-//GET blogs
-app.get("/blogs", (req, res) => {
-	const blogs = Blog.find()
-		.then(blogs => {
-			res.json(blogs);
-		})
-		.catch(error => {
-			res.json({ message: "Unable to get blogs:", error });
-		});
+// GET blogs
+app.get("/blogs", async (req, res) => {
+	try {
+		const blogs = await Blog.find();
+		res.status(200).json(blogs);
+	} catch (error) {
+		console.error("Error fetching blogs:", error);
+		res.status(500).json({ message: "Unable to get blogs", error });
+	}
 });
 
-//GET blogs by ID
-app.get("/blogs/:id", (req, res) => {
+// GET blog by ID
+app.get("/blogs/:id", async (req, res) => {
 	const { id } = req.params;
-	const singleBlog = Blog.findById(id)
-		.then(singleBlog => {
-			res.json(singleBlog);
-		})
-		.catch(error => {
-			res.json({ message: "Unable to get blog by ID:", error });
-		});
+	try {
+		const singleBlog = await Blog.findById(id);
+		if (singleBlog) {
+			res.status(200).json(singleBlog);
+		} else {
+			res.status(404).json({ message: "Blog not found" });
+		}
+	} catch (error) {
+		console.error("Error fetching blog by ID:", error);
+		res.status(500).json({ message: "Unable to get blog by ID", error });
+	}
 });
 
-//POST blog
-app.post("/blogs", (req, res) => {
+// POST blog
+app.post("/blogs", async (req, res) => {
 	const { title, preview, post } = req.body;
 	const blog = new Blog({ title, preview, post });
-	blog
-		.save()
-		.then(blog => {
-			res.json({ message: "Blog was CREATED successfully" });
-		})
-		.catch(error => {
-			res.json({ message: "Unable to post blog:", error });
-		});
+	try {
+		const savedBlog = await blog.save();
+		res
+			.status(201)
+			.json({ message: "Blog was CREATED successfully", blog: savedBlog });
+	} catch (error) {
+		console.error("Error posting blog:", error);
+		res.status(400).json({ message: "Unable to post blog", error });
+	}
 });
 
-//UPDATE blog
-app.put("/blogs/:id", (req, res) => {
+// UPDATE blog
+app.put("/blogs/:id", async (req, res) => {
 	const { id } = req.params;
 	const { title, preview, post } = req.body;
-	const updatedBlog = Blog.findByIdAndUpdate(
-		id,
-		{ title, preview, post },
-		{ value: true }
-	)
-		.then(updatedBlog => {
-			res.json({ message: "Blog was successfully UPDATED" });
-		})
-		.catch(error => {
-			res.json({ message: "Unable to update blog:", error });
-		});
+	try {
+		const updatedBlog = await Blog.findByIdAndUpdate(
+			id,
+			{ title, preview, post },
+			{ new: true }
+		);
+		if (updatedBlog) {
+			res
+				.status(200)
+				.json({ message: "Blog was successfully UPDATED", blog: updatedBlog });
+		} else {
+			res.status(404).json({ message: "Blog not found" });
+		}
+	} catch (error) {
+		console.error("Error updating blog:", error);
+		res.status(400).json({ message: "Unable to update blog", error });
+	}
 });
 
-//DELETE blog
-app.delete("/blogs/:id", (req, res) => {
+// DELETE blog
+app.delete("/blogs/:id", async (req, res) => {
 	const { id } = req.params;
-	const deletedBlog = Blog.findByIdAndDelete(id)
-		.then(deletedBlog => {
-			res.json({ message: "Blog was successfully DELETED" });
-		})
-		.catch(error => {
-			res.json({ message: "Unable to delete blog:", error });
-		});
+	try {
+		const deletedBlog = await Blog.findByIdAndDelete(id);
+		if (deletedBlog) {
+			res.status(200).json({ message: "Blog was successfully DELETED" });
+		} else {
+			res.status(404).json({ message: "Blog not found" });
+		}
+	} catch (error) {
+		console.error("Error deleting blog:", error);
+		res.status(500).json({ message: "Unable to delete blog", error });
+	}
 });
